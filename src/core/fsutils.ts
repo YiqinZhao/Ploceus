1abeca1c1e5fd8be2380b67c258e36ffee67ca68
Yiqin Zhao <yiqinzhao@outlook.com>
2021-04-12 11:36:15 -0400

    :sparkles: Switching to PrismJS

--- src/core/fsutils.ts
+++ src/core/fsutils.ts
@@ -5,21 +5,75 @@ import CryptoJS from "crypto-js"
 import matter from "gray-matter"
 
 import { Ploceus } from "."
-import hljs from "highlight.js"
+
+import Prism from "prismjs"
+import prism from "markdown-it-prism"
 import markdownIt from "markdown-it"
 
 const md: any = markdownIt({
     html: true,
-    highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-            try {
-                return '<pre class="hljs"><code>' +
-                    hljs.highlight(lang, str, true).value +
-                    '</code></pre>'
-            } catch (__) { }
-        }
+}).use(prism, {
+    init: (prism: typeof Prism) => {
+        var latex_cmd = /\\(?:[`~@#$%^&()_=+{}\[\]\|\\:;"',\?\/]|[a-z]+\*?(?=\s*{?))/mi;
+        var internal_tags = {
+            'equation': {
+                pattern: /(\$\$?)[^\$]+\1/mi,
+                alias: 'function italic',
+                inside: {
+                    'tag': latex_cmd
+                }
+            },
+            'tag': latex_cmd
+        };
 
-        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>'
+        prism.languages.bib = {
+            'comment': /%.*/,
+            'special': {
+                pattern: /(^\s*)@(?:(?:preamble|string(?=\s*[({]))|comment(?=\s*[{]))/mi,
+                lookbehind: true,
+                alias: 'important'
+            },
+            'type': {
+                pattern: /(^\s*)@[^,={}'"\s]+(?=\s*{)/mi,
+                lookbehind: true,
+                alias: 'function bold'
+            },
+            'name': {
+                pattern: /([,{]\s*)[^,={}'"\s]+(?=\s*[,}])/mi,
+                lookbehind: true,
+                alias: 'regex'
+            },
+            'field': {
+                pattern: /([,{(]\s*)[^,={}'"\s]+(?=\s*=)/mi,
+                lookbehind: true,
+                alias: 'keyword'
+            },
+            'number': {
+                pattern: /(=\s*)[0-9]+(?=\s*[,}])/mi,
+                lookbehind: true,
+                alias: 'char'
+            },
+            'value': {
+                pattern: /([=#]\s*){(?:[^{}]*|{(?:[^{}]*|{(?:[^{}]*|{[^}]*})*})*})*}/mi,
+                lookbehind: true,
+                alias: 'char',
+                greedy: true,
+                inside: internal_tags
+            },
+            'ref-string': {
+                pattern: /([=#]\s*)[^,={}'"\s]+(?=\s*[#,}])/mi,
+                lookbehind: true,
+                alias: 'keyword'
+            },
+            'char': {
+                pattern: /("|')(?:(?!\1)[^\\]|\\(?:\r\n|[\s\S]))*\1/mi,
+                greedy: true,
+                inside: internal_tags
+            },
+            'symbol': /#/,
+            'punctuation': /[=,{}]/
+        };
+        prism.languages.bibtex = prism.languages.bib
     }
 }).use(require('@iktakahiro/markdown-it-katex'))
     .use(require('markdown-it-anchor'))
